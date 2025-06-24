01d496a2bd976ab715257e580a5fb1b0159219ac
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2025-06-24 16:26:01 +0000

    https [lambda]

--- terraform/modules/fargate/main.tf
+++ terraform/modules/fargate/main.tf
@@ -149,8 +149,8 @@ resource "aws_lb_target_group" "app" {
 # ALB Listener
 resource "aws_lb_listener" "app" {
   load_balancer_arn = aws_lb.app.arn
-  port              = "80"
-  protocol          = "HTTP"
+  port              = "443"
+  protocol          = "HTTPS"
   
   default_action {
     type             = "forward"
